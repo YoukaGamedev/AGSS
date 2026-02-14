33ecf58a59c7aad7d2284ac79f338957c7f89b87
YoukaGamedev <rizzcool13@gmail.com>
2026-02-14 21:20:50 +0700

    Update Game.php

--- godot-game-hub/app/Models/Game.php
+++ godot-game-hub/app/Models/Game.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     protected $fillable = [
-        'title', 'description', 'thumbnail', 'zip_file', 'html_file', 'user_id'
+        'title',
+        'description',
+        'thumbnail', 
+        'zip_file', 
+        'html_file', 
+        'user_id'
     ];
 
     public function user()
